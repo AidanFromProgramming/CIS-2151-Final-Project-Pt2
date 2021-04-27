f9eb61b6f507c853ae9c4ae74aa24e568799c9c5
Aidan <aidanfromprogramming@gmail.com>
2021-04-27 12:30:29 -0400

    Inital ui commit
    Signed-off-by: Aidan <aidanfromprogramming@gmail.com>

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,4 +1,30 @@
 package ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
 public class Controller {
+    
+    public TextField game_save_path_entry;
+    public Button game_load_button;
+    public Button load_game_option_button;
+    public Button new_game_option_button;
+    public TextField game_save_path;
+
+
+    @FXML
+    public void initialize() {
+    }
+
+
+    public void load_game_requested(MouseEvent mouseEvent) {
+    }
+
+    public void new_game_requested(MouseEvent mouseEvent) {
+    }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("ui.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Blackjack");
+        primaryStage.setScene(new Scene(root, 1280, 720));
         primaryStage.show();
     }
 
