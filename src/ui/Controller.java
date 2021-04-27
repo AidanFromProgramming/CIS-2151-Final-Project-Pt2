92589701da0690d1214a03cb00d73e1e75d8628e
Aidan <aidanfromprogramming@gmail.com>
2021-04-27 12:39:54 -0400

    added file explorer
    Signed-off-by: Aidan <aidanfromprogramming@gmail.com>

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -7,6 +7,10 @@ import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.*;
+
+import java.io.File;
 
 public class Controller {
     
@@ -15,14 +19,27 @@ public class Controller {
     public Button load_game_option_button;
     public Button new_game_option_button;
     public TextField game_save_path;
+    public AnchorPane main_stage;
 
+    File gameFile = null;
 
     @FXML
     public void initialize() {
+
+
     }
 
 
     public void load_game_requested(MouseEvent mouseEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select game save");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Blackjack Saves", "*.blkjk"),
+                new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showOpenDialog(main_stage.getScene().getWindow());
+//        if (selectedFile != null) {
+//            .display(selectedFile);
+//        }
     }
 
     public void new_game_requested(MouseEvent mouseEvent) {
